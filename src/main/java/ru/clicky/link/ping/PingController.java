e4975d11cec1e8cf71862a94174066690ab847b6
Daniil Lebedev <lebedevdanka4@gmail.com>
2026-08-25 18:29:38 +0300

    fix/missed-map-method-ping

--- src/main/java/ru/clicky/link/ping/PingController.java
+++ src/main/java/ru/clicky/link/ping/PingController.java
@@ -1,5 +1,6 @@
 package ru.clicky.link.ping;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ru.clicky.link.common.response.MessageResponse;
 
@@ -7,6 +8,7 @@ import ru.clicky.link.common.response.MessageResponse;
 public class PingController {
   private static final String PING_MESSAGE = "OK";
 
+  @GetMapping("/ping")
   MessageResponse ping(){
     return new MessageResponse(PING_MESSAGE);
   }
