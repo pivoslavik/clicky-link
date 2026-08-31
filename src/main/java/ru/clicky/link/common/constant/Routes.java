185540e8961ec2ed5a10ad4399eb5900f92d8d6f
Daniil Lebedev <151195502+pivoslavik@users.noreply.github.com>
2026-08-31 14:28:05 +0300

    feature/rate-limiter (#16)

--- src/main/java/ru/clicky/link/common/constant/Routes.java
+++ src/main/java/ru/clicky/link/common/constant/Routes.java
@@ -3,4 +3,5 @@ package ru.clicky.link.common.constant;
 public final class Routes {
   public static final String REDIRECT = "/r/{slug}";
   public static final String LINK = "/link";
+  public static final String PROCESS = "/process";
 }
