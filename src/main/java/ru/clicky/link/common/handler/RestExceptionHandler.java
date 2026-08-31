9739b5b8757450bba349315722b2edf7b366179d
Daniil Lebedev <151195502+pivoslavik@users.noreply.github.com>
2026-08-31 11:57:43 +0300

    feature/base-frontend-webpage (#15)

--- src/main/java/ru/clicky/link/common/handler/RestExceptionHandler.java
+++ src/main/java/ru/clicky/link/common/handler/RestExceptionHandler.java
@@ -1,5 +1,7 @@
 package ru.clicky.link.common.handler;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,18 +15,23 @@ import ru.clicky.link.core.LinkNotFoundException;
 
 @RestControllerAdvice
 public class RestExceptionHandler {
+  private static final Logger logger = LoggerFactory.getLogger(RestExceptionHandler.class);
+
   @ExceptionHandler(LinkAlreadyExistsException.class)
   public ResponseEntity<MessageResponse> handleBadRequestException(RuntimeException e) {
+    logger.warn(e.getMessage(), e);
     return ResponseEntity.badRequest().body(new MessageResponse(e.getMessage()));
   }
 
   @ExceptionHandler(LinkNotFoundException.class)
   public ResponseEntity<MessageResponse> handleNotFoundException(RuntimeException e) {
+    logger.warn(e.getMessage(), e);
     return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new MessageResponse(e.getMessage()));
   }
 
   @ExceptionHandler(MethodArgumentNotValidException.class)
   public ResponseEntity<MessageResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+    logger.warn(e.getMessage(), e);
     FieldError firstError = e.getBindingResult().getFieldError();
     String defaultMessage = (firstError != null) ? firstError.getDefaultMessage() : "Validation Error";
     return ResponseEntity.badRequest().body(new MessageResponse(defaultMessage));
@@ -32,11 +39,13 @@ public class RestExceptionHandler {
 
   @ExceptionHandler(DataIntegrityViolationException.class)
   public ResponseEntity<MessageResponse> handleDataIntegrityViolationException(DataIntegrityViolationException e) {
+    logger.warn(e.getMessage(), e);
     return ResponseEntity.badRequest().body(new MessageResponse("Error processing the database request. Please try again later"));
   }
 
   @ExceptionHandler(Exception.class)
   public ResponseEntity<MessageResponse> handleException(Exception e) {
+    logger.error(e.getMessage(), e);
     return ResponseEntity.internalServerError().body(new MessageResponse("An internal server error occurred. Please try again later"));
   }
 }
