a99651bc6414d852e4fc701d29a861c7051d3bf8
Daniil Lebedev <lebedevdanka4@gmail.com>
2026-08-31 11:55:38 +0300

    feature/base-frontend-webpage

---
+++ src/main/java/ru/clicky/link/ui/FormPage.java
@@ -0,0 +1,34 @@
+package ru.clicky.link.ui;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import ru.clicky.link.core.LinkCreateRequest;
+import ru.clicky.link.core.LinkService;
+
+@Controller
+public class FormPage {
+  private final LinkService linkService;
+
+  @Value("${app.backend-url}")
+  private String backendUrl;
+
+  public FormPage(LinkService linkService) {
+    this.linkService = linkService;
+  }
+
+  @GetMapping("/")
+  public String index() {
+    return "index";
+  }
+
+  @PostMapping("/process")
+  public String processForm(@ModelAttribute LinkCreateRequest request, Model model) {
+    model.addAttribute("linkInfo", linkService.createShortLink(request));
+    model.addAttribute("baseUrl", backendUrl);
+    return "index :: result-block";
+  }
+}
